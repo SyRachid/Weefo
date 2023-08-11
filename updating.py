06ae82b202549d31b594c82b7f5e1077b2de71c5
Reine2001 <eulaliethiombiano@gmail.com>
2023-08-11 09:29:02 +0000

    mise à jour

---
+++ updating.py
@@ -0,0 +1,27 @@
+from main import session
+from models import Admin, Livreur, Client, Livraison
+
+def update_livreur(livreur_id, new_username):
+    livreur = session.query(Livreur).filter_by(id=livreur_id).first()
+    if livreur:
+        livreur.username = new_username
+        session.commit()
+
+
+def update_client(client_id, new_username):
+    client = session.query(Client).filter_by(id=client_id).first()
+    if client:
+        client.username = new_username
+        session.commit()
+
+def update_livraison(livraison_id, new_etatLivraison):
+    livraison = session.query(Livraison).filter_by(id=livraison_id).first()
+    if livraison:
+       livraison.etatLivraison = new_etatLivraison
+       session.commit()
+
+def update_admin(admin_id, new_username):
+    admin = session.query(Admin).filter_by(id=admin_id).first()
+    if admin:
+        admin.username = new_username
+        session.commit()
