838c95f18e87e13486be570a514ef2b0a789b203
SyRachid <syrachidsanogo78@gmail.com>
2023-08-13 08:52:48 +0000

    ajout des fonctionnalités du client

---
+++ client_functions.py
@@ -0,0 +1,26 @@
+from database_utils import session, Admin, Livreur, Client, Livraison
+import logging
+
+logging.getLogger('sqlalchemy').setLevel(logging.WARNING)
+
+def Livre_moi():
+    id = input("entrez votre Id pour la livraison")
+    id_ = int(id)
+    creer_nouvelle_livraison(id_,"en cours")
+
+def creer_nouvelle_livraison(client_id, etatLivraison):
+    client = session.query(Client).get(client_id)
+    if client:
+        livreur = session.query(Livreur).filter_by(disponibilite = "OUI").first()
+        if livreur:
+            nouvelle_livraison = Livraison(etatLivraison=etatLivraison, admin=client.admin, livreur=livreur, client=client)
+            livreur.disponibilite = "non"
+            session.add(nouvelle_livraison)
+            session.commit()
+            print("Nouvelle livraison créée avec succès!")
+        else:
+            print("Aucun livreur disponible pour le moment.")
+    else:
+        print("Client non trouvé.")
+
+# Ajoutez ici d'autres fonctions pour gérer les opérations des clients
